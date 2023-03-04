e8f5251d8e5c52da44300a1aea9520576f4bc992
RickKoenig <mkoeni@sbcglobal.net>
2023-03-03 20:52:22 -0800

    start work on line fractals

--- lineFractal/mainApp.js
+++ lineFractal/mainApp.js
@@ -47,9 +47,8 @@ class MainApp {
 		this.oldTime; // for delta time
 		this.avgFpsObj = new Runavg(500);
 
-		this.pntRad = .05; // size of point
+		this.pntRad = .02; // size of point
 		this.pnts = [[0, 0], [7/8, 1/8], [1, 1], [9/8, 1/8], [2, 0]];
-		this.numPnts = this.pnts.length;
 
 		// interactive edit of points
 		this.editPnts = new EditPnts(this.pnts, this.pntRad);
@@ -57,13 +56,31 @@ class MainApp {
 		// before firing up Plotter2d
 		this.startCenter = [.5, .5];
 		this.startZoom = .5;
+	}
 
-		this.fractalDimension = 1.87;
+	#calcFractalDimension(pnts) {
+		return pnts.length * 1.1;
+
 	}
 
 	#userBuildUI() {
 		makeEle(this.vp, "hr");
 		this.eles.textInfoLog = makeEle(this.vp, "pre", null, null, "textInfoLog");
+		// line step slider combo
+		{
+			makeEle(this.vp, "hr");
+			// start depth UI
+			const label = "Fractal depth";
+			const min = 0;
+			const max = 8;
+			const start = 2;
+			const step = 1;
+			const precision = 0;
+			new makeEleCombo(this.vp, label, min, max, start, step, precision,  (v) => {
+				this.depth = v;
+				this.dirty = true;});
+			// end depth UI
+		}
 	}		
 	
 	#userProc() {
@@ -81,32 +98,49 @@ class MainApp {
 		this.avgFps = this.avgFpsObj.add(this.fps);
 		// pass in the buttons and the user/cam space mouse from drawPrim
 		this.dirty = this.editPnts.proc(this.input.mouse, this.plotter2d.userMouse) || this.dirty;
+	}
 
+	#drawFractal(pnts, level) {
+		//this.depth;
+		// draw lines between points
+		if (level > 0) {
+			for (let i = 0; i < pnts.length - 1; ++i) {
+				const p0 = pnts[i];
+				const p1 = pnts[i + 1];
+				this.drawPrim.drawLine(p0, p1, .005, "black");
+				const deepPnts = [];
+				for (let j = 0; j < pnts.length; ++j) {
+					const pd = vec2.clone(pnts[j]);
+					vec2.scale(pd, pd, .25);
+					let p = vec2.clone(p0);
+					vec2.add(p, p, pd);
+					deepPnts.push(p);
+				}
+				this.#drawFractal(deepPnts, level - 1);
+				//midPnt(mid, p0, p1);
+				//this.drawPrim.drawCircleO(mid, .05, undefined, "magenta");
+			}
+		}
 	}
 
 	#userDraw() {
 		// draw with hilits on some points
 		const hilitPntIdx = this.editPnts.getHilitIdx();
-		for (let i = 0; i < this.numPnts; ++i) {
+		for (let i = 0; i < this.pnts.length; ++i) {
 			this.drawPrim.drawCircle(this.pnts[i], this.pntRad, "green");
 			let doHilit = i == hilitPntIdx;
 			this.drawPrim.drawCircleO(this.pnts[i], this.pntRad, .01, doHilit ? "yellow" : "black");
 		}
-		// draw lines between points
-		for (let i = 0; i < this.numPnts - 1; ++i) {
-			const p0 = this.pnts[i];
-			const p1 = this.pnts[(i + 1) % this.numPnts];
-			this.drawPrim.drawLine(p0, p1, "darkgray");
-			//midPnt(mid, p0, p1);
-			//this.drawPrim.drawCircleO(mid, .05, undefined, "magenta");
-		}
+		this.#drawFractal(this.pnts, this.depth);
 	}
 
 	// USER: update some of the UI in vertical panel if there is some in the HTML
 	#userUpdateInfo() {
-		let countStr = "Dirty Count = " + this.dirtyCount
-		countStr += "\nAvg fps = " + this.avgFps.toFixed(2)
+		this.fractalDimension = this.#calcFractalDimension(this.pnts);
+		let countStr = "Dirty Count = " + this.dirtyCount;
+		countStr += "\nAvg fps = " + this.avgFps.toFixed(2);
 		countStr += "\nFractal dimension = " + this.fractalDimension.toFixed(3);
+		countStr += "\nFractal draw depth = " + this.depth;
 		this.eles.textInfoLog.innerText = countStr;
 	}
 
@@ -145,10 +179,6 @@ class MainApp {
 
 		// keep animation going
 		requestAnimationFrame(() => this.#animate());
-	}
-
-	#resetCounter() {
-		this.count = 0;
 	}
 }
 
