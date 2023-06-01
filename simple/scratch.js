346b934db293982df4ae0fe4864f43857caf3e1c
RickKoenig <mkoeni@sbcglobal.net>
2023-05-31 20:49:20 -0700

    study of repeating decimals

--- simple/scratch.js
+++ simple/scratch.js
@@ -2,22 +2,48 @@
 
 // do division by hand looking for repeating decimals
 function repCalc(d) {
-    let numDigits = 30;
-    let repStr = "0.";
-    let r = 10; // shift right for numbers to the right of decimal point
+    let numDigits = 10;
+    let decimalStr = "0.";
+    let r = 1; // shift right for numbers to the right of decimal point
+    const remArray = [r]; // remainders
+    let qStr = "";
+    let qStartStr = "";
+    let qRepStr = "";
+    let finishStr = "what";
     while(r && numDigits--) {
+        r *= 10;
         const q = Math.floor(r / d);
         r -= q * d;
-        r *= 10;
-        repStr = repStr.concat(q);
+        qStr += q;//decimalStr.concat(q);
+        if (!r) {
+            finishStr = " T";
+            break;
+        }
+        const idx = remArray.indexOf(r);
+        if (idx >= 0) {
+            finishStr = ` R ${idx}`;
+            break;
+        }
+        remArray.push(r);
     }
-    return repStr;
+
+    qRepStr = qStr;
+
+    return decimalStr + qStartStr + "[" + qRepStr + "]" + finishStr;
+}
+
+function repDecimalTests() {
+    console.log("repeat decimal tests");
+    for (let d = 2; d < 14; ++d) {
+        const decimalStr = repCalc(d);
+        console.log(`den = ${d.toString().padStart(3)}, decimalStr = ${decimalStr}`);
+    }
 }
 
 function runScratch() {
     console.log("DOING SCRATCH!");
-    for (let d = 2; d < 30; ++d) {
-        const repStr = repCalc(d);
-        console.log(`den = ${d.toString().padStart(3)}, repStr = ${repStr}`);
-    }
+    const arr = [3, 5, 7, 11];
+    const idx = arr.indexOf(11);
+    console.log(`find = ${idx}`);
+    repDecimalTests();
 }
