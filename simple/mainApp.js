186a1c39472f6b95c7a41810275bf737d8bd2e3b
RickKoenig <mkoeni@sbcglobal.net>
2024-07-28 09:37:54 -0700

    got proper control points working

--- simple/mainApp.js
+++ simple/mainApp.js
@@ -154,9 +154,9 @@ class MainApp {
 		// boost controls
 		const bMatBoost = [
 			[0, 1, 0, 0],
-			[2, -2, 0, 0],
-			[-4, 1, 1, 2],
-			[2, 0, 0, -2]
+			[3, -3, 0, 0],
+			[-6, 3, 0, 3],
+			[3, -1, 1, -3]
 		];
 		// normal controls
 		const bMatNormal = [
