482832caab3ae106ab918d7ae9b420ae9ec8f6e6
RickKoenig <mkoeni@sbcglobal.net>
2025-05-07 20:50:26 -0700

    variable length exp field

--- QBoxEigen/posits.js
+++ QBoxEigen/posits.js
@@ -1,29 +1,39 @@
 'use strict';
 
 class positBits {
-    constructor(numBits, bp) {
+    constructor(numBits, numExpBits, bp) {
         this.numBits = numBits;
+        this.numExpBits = numExpBits;
         this.bp = bp;
 
         // print early before bits get mangled
         this.#print();
         // get sign bit
-        const s = this.#getBits(1);
-        let r, e, f;
-        let k;
-        this.rat;
+        const sign = this.#getBits(1);
         if (this.bp == 0) { // special cases for 0 and infinity
             // get rational for special cases
-            this.rat = s ? "NaR" : 0;
+            this.rat = sign ? "NaR" : 0;
             this.rat += " special";
         } else {
             // get regime, unary coding
-            const firstBit = this.#peekBits(1);
-            // get exponent, 2 bits
+            const {fb, n, k, r}  = this.#getRegime();
+            // get exponent, usually 2 bits
+            const e = this.#getBits(this.numExpBits);
             // get fraction
-            console.log( "      s = " + s + ", firstbit = " + firstBit + ", k = " + k + ", r = " + r + ", e = " + e + ", f = " + f);
+            const f = this.bp / Math.pow(2, this.numBits);
+            console.log("      sign = " + sign
+                 + ", fb = " + fb
+                 + ", k = " + k 
+                 + ", r = " + r 
+                 + ", n = " + n
+                 + ", e = " + e 
+                 + ", f = " + "frac bits left = " + this.numBits + ", bp = " + this.bp + ", f = " + f
+                );
             // get rational
-            this.rat = bp;
+            const mant = (1 - 3 * sign) + f; // check associative property dependence
+            const rMult = 1 << this.numExpBits;
+            const exp = (1 - 2 * sign) * (rMult * r + e + sign);
+            this.rat = mant * Math.pow(2, exp);
         }
     }
 
@@ -40,7 +50,7 @@ class positBits {
     #peekBits(nb) {
         if (nb < this.numBits) { // more than enough bits
             const rShift = this.numBits - nb;
-            const mask = (1 << rShift) - 1;
+            const mask = (1 << nb) - 1;
             const ret = (this.bp >> rShift) & mask;
             return ret;
         } else if (nb == this.numBits) { // match
@@ -55,13 +65,41 @@ class positBits {
     #getBits(nb) {
         const ret = this.#peekBits(nb);
         const rShift = Math.min(this.numBits, nb);
-        //this.bp >>= rShift;
         this.numBits -= rShift;
         const mask = (1 << this.numBits) - 1;
         this.bp &= mask;
         return ret;
     }
-    
+
+    #getRegime() {
+        // assume bp != 0
+        // unary coding
+        // first bit
+        const fb = this.#getBits(1);
+        let k = 1;
+        let watch = 0;
+        while(true) {
+            const nextBit = this.#getBits(1);
+            if (fb != nextBit) {
+                break; // bit flipped, done
+            }
+            ++k;
+            ++watch;
+            if (watch == 100) {
+                throw("'watch hit, getRegime'");
+            }
+        }
+        const n = this.numBits; // how many bits left
+        let r = fb ? k - 1 : -k;
+        const ret = {
+            fb: fb,
+            n: n,
+            k: k,
+            r: r
+        }
+        return ret;
+    }
+
     #print() {
         const bpStr = this.#bpToStr();
         console.log("   PRINT POSIT int value of bp = " + this.bp 
@@ -73,20 +111,41 @@ class positBits {
     }
 }
 
-function analyzePosits(numBits, bp) {
-    const posit = new positBits(numBits, bp);
+function analyzePosits(numBits, numExpBits, bp) {
+    const posit = new positBits(numBits, numExpBits, bp);
     const ratVal = posit.getRatVal();
     console.log("      rat value = " + ratVal);
 }
 
 function doPosits() {
     console.log("doing posits");
+    console.log("5 bit posits, exp bits = 1");
+    for (let bp = 0; bp < (1 << 5); ++bp) {
+        analyzePosits(5, 1, bp);
+    }
+    console.log("done 5 bit posits, exp bits = 1\n");
+    const numExpBits = 2;
+    //analyzePosits(6, numExpBits, 45);
+    analyzePosits(9, numExpBits, 203);
+    analyzePosits(9, numExpBits, -203);
     const minBits = 1;
     const maxBits = 3;
+    const straight = false; // or alternating
     for (let numBits = minBits; numBits <= maxBits; ++numBits) {
+        const maxBp = 1 << numBits;
         console.log("numBits = " + numBits);
-        for (let bp = 0; bp < 1 << numBits; ++bp) {
-            analyzePosits(numBits, bp);
+        if (straight) {
+            for (let bp = 0; bp < maxBp; ++bp) {
+                analyzePosits(numBits, numExpBits, bp);
+            }
+        } else { // alternate
+            let bp = 0;
+            analyzePosits(numBits, bp); // special 0
+            for (bp = 1; bp < maxBp / 2; ++ bp) {
+                analyzePosits(numBits, numExpBits, bp);
+                analyzePosits(numBits, numExpBits, -bp);
+            }
+            analyzePosits(numBits, numExpBits, bp); // special NaR
         }
         console.log("");
     }
