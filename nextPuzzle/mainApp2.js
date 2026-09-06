b0c2ed95d5571fb5988567bcd140bbc7fc6c8134
Rick Koenig <mkoeni@sbcglobal.net>
2026-09-05 19:42:37 -0700

    work on piece shapes, tee, el, square, etc

--- nextPuzzle/mainApp2.js
+++ nextPuzzle/mainApp2.js
@@ -2,10 +2,24 @@
 
 class Piece {
 	// TODO: add piece type/shape
-	constructor(o, pieceSize) {
+	constructor(o, pieceSize, shapeData) {
 		this.pos = o.pos;
 		this.color = o.color;
+		this.shapeData = shapeData;
 		this.pieceSize = pieceSize;
+		this.minPoint = vec2.clone(shapeData[0]);
+		this.maxPoint = vec2.clone(shapeData[0]);
+		for (let i = 1; i < shapeData.length; ++i) {
+			const sd = shapeData[i];
+			vec2.min(this.minPoint, this.minPoint, sd);
+			vec2.max(this.maxPoint, this.maxPoint, sd);
+		}
+	}
+	// keep piece in range of board
+	range(boardX, boardY, pos) {
+		pos[0] = range(-this.minPoint[0], pos[0], boardX - 1 - this.maxPoint[0]);
+		pos[1] = range(-this.minPoint[1], pos[1], boardY - 1 - this.maxPoint[1]);
+		return pos;
 	}
 
 	draw(user, hilit) {
@@ -13,15 +27,23 @@ class Piece {
 		const smallWidth = .015;
 		const bigWidth = .08;
 		const darkCol = "#0004";
-		user.drawPrim.drawRectangleCenter(this.pos, smallerRad, this.color);
-		user.drawPrim.drawRectangleCenterO(this.pos, smallerRad, bigWidth, hilit ? "black" : darkCol);
-		user.drawPrim.drawRectangleCenterO(this.pos, smallerRad, smallWidth, "white");
+		const sqPos = vec2.create();
+		for (const offsetPos of this.shapeData) {
+			vec2.add(sqPos, offsetPos, this.pos);
+			user.drawPrim.drawRectangleCenter(sqPos, smallerRad, this.color);
+			user.drawPrim.drawRectangleCenterO(sqPos, smallerRad, bigWidth, hilit ? "black" : darkCol);
+			user.drawPrim.drawRectangleCenterO(sqPos, smallerRad, smallWidth, "white");
+		}
+		user.drawPrim.drawRectangleCenter(this.pos, [this.pieceSize / 4, this.pieceSize / 4], "white");
+		user.drawPrim.drawRectangleCenterO(this.pos, [this.pieceSize / 4, this.pieceSize / 4], smallWidth, "black");
+		const textSize = .05;
+	    user.drawPrim.drawText(this.pos, [textSize, textSize], "M");
 	}
 }
 
 class PieceContainer {
-	// pieces are on even 'x' numbers from -8 to +8
-	constructor(user, pieceData, boardX, boardY, pieceSize) {
+	// pieces are on whole numbers 0, 0 to boardX -1, boardY - 1
+	constructor(user, pieceData, pieceShapes, boardX, boardY, pieceSize) {
 		this.pieceSize = pieceSize;
 		this.statesEnumStrs = ["IDLE", "DRAGGING"];
 		this.boardX = boardX;
@@ -32,7 +54,8 @@ class PieceContainer {
 
 		this.container = [];
 		for (const po of pieceData) {
-			const p = new Piece(po, pieceSize);
+			const shapeData = pieceShapes[po.shapeIdx];
+			const p = new Piece(po, pieceSize, shapeData);
 			this.container.push(p);
 		}
 
@@ -99,18 +122,17 @@ class PieceContainer {
 		switch(this.state) {
 			// MOVE piece
 			case this.statesEnum.DRAGGING:
-				const curPiece = this.container[this.idx].pos;
+				const pce = this.container[this.idx];
+				const curPos = pce.pos;
 				let mousePos = fmxy;
 				this.endPos = vec2.clone(mousePos);
 				// keep within bounds of the board
-				this.startPos = vec2.clone(curPiece);
-				this.endPos[0] = range(0, this.endPos[0], this.boardX - 1);
-				this.endPos[1] = range(0, this.endPos[1], this.boardY - 1);
-				vec2.copy(curPiece, this.endPos); // default, if no collisions
+				this.startPos = vec2.clone(curPos);
+				this.endPos = pce.range(this.boardX, this.boardY, this.endPos); // keep the piece on the board
 				const avoidLocs = this.#makeAvoidPieces(); // take container of pieces and remove self and just make arr of pos
 				const newPos = solvePath(
 					this.startPos, this.endPos, avoidLocs, this.pieceSize, this.user.slow, this.user.solveSpeed);
-				vec2.copy(curPiece, newPos); // update container with curPiece REFERENCE
+				vec2.copy(curPos, newPos); // update container with curPiece REFERENCE
 				break;
 		}
 	}
@@ -188,10 +210,9 @@ class MainApp2 {
 
 		// fire up all instances of the classes that are needed
 		// vp (vertical panel) is for UI trans, scale info, reset and USER
-		
 		const safe = .25;
 		const safeBottomFactor = 6;
-		const safeBottom = this.boardY / safeBottomFactor; // add some more safe at the bottom for mobile devices
+		const safeBottom = .5 * this.boardY / safeBottomFactor; // add some more safe at the bottom for mobile devices
 		const extraX = this.boardX / 2;
 		const extraY = this.boardY / 2;
 		this.startCenter = [this.boardX / 2 - .5, this.boardY / 2 - .5 - safeBottom / 2];
@@ -212,13 +233,6 @@ class MainApp2 {
 		this.#animate();
 	}
 
-	#randomColor() {
-		const r = getRandomInt(256);
-		const g = getRandomInt(256);
-		const b = getRandomInt(256);
-		this.vp.style.background = `rgb(${r}, ${g}, ${b}`;
-	}
-
 	#initBoard() {
 		this.board = new Board(this, this.boardX, this.boardY);
 	}
@@ -226,15 +240,55 @@ class MainApp2 {
 	#initPieces() {
 		// slide objects and container
 		this.pieceSize = .875;
-		// build board
-		const pieceDataArr = [
-			{pos: [2, 1], color: "red"},
-			{pos: [4, 2], color: "green"},
-			{pos: [6, 1], color: "blue"},
-			{pos: [2, 4], color: "yellow"},
-			{pos: [6, 4], color: "peru"},
+		// build pieces
+		const pieceShapeEnums = makeEnum(["sq1", "sq2", "sq3", "tee", "el"]);
+		const pieceShapes = [
+			// sq1
+			[
+				[0, 0]
+			],
+			// sq2
+			[
+				[0, 0],
+				[0, 1],
+				[1, 0],
+				[1, 1]
+			],
+			// sq3
+			[
+				[-1, -1],
+				[-1, 0],
+				[-1, 1],
+				[0, -1],
+				[0, 0],
+				[0, 1],
+				[1, -1],
+				[1, 0],
+				[1, 1]
+			],
+			// tee
+			[
+				[-1, 0],
+				[0, 0],
+				[1, 0],
+				[0, -1]
+			],
+			// el
+			[
+				[0, 2],
+				[0, 1],
+				[0, 0],
+				[1, 0]
+			],
 		];
-		this.pieceContainer = new PieceContainer(this, pieceDataArr, this.boardX, this.boardY, this.pieceSize);
+		const pieceDataArr = [
+			{pos: [2, 1], color: "red", shapeIdx: pieceShapeEnums.sq1},
+			{pos: [4, 2], color: "green", shapeIdx: pieceShapeEnums.sq3},
+			{pos: [6, 1], color: "blue", shapeIdx: pieceShapeEnums.el},
+			{pos: [2, 4], color: "yellow", shapeIdx: pieceShapeEnums.tee},
+			{pos: [6, 4], color: "peru", shapeIdx: pieceShapeEnums.sq1},
+		];
+		this.pieceContainer = new PieceContainer(this, pieceDataArr, pieceShapes, this.boardX, this.boardY, this.pieceSize);
 		this.winner = false;
 		this.loser = false;
 	}
@@ -319,7 +373,7 @@ class MainApp2 {
 			// show line where we would like to go
 			if (isDragging) {
 				const curPnt = this.pieceContainer.container[this.pieceContainer.idx];
-				this.drawPrim.drawLine(pntM, curPnt, .025, "black");
+				this.drawPrim.drawLine(pntM, curPnt.pos, .025, "black");
 				this.drawPrim.drawCircle(pntM, .05, "green");
 			}
 		}
@@ -344,6 +398,13 @@ class MainApp2 {
 		infoStr += "\nstate = " + this.pieceContainer.statesEnumStrs[this.pieceContainer.state];
 		infoStr += "\n\n";
 		this.eles.textInfoLog.innerText = infoStr;
+	}
+
+	#randomColor() {
+		const r = getRandomInt(256);
+		const g = getRandomInt(256);
+		const b = getRandomInt(256);
+		this.vp.style.background = `rgb(${r}, ${g}, ${b}`;
 	}
 
 	// proc
